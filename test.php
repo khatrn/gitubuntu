cc67d8318fca26e2733cbfbd1eca698fda910b3f
khatn <kha.tn@neo-lab.vn>
2020-12-18 16:50:11 +0700

    [add][master]Add paragraph div into website

--- test.php
+++ test.php
@@ -3,5 +3,9 @@
 <head>
 </head>
 <body style="font-family:Verdana;">
+<div style="background-color:#f1f1f1;padding:15px;">
+  <h1>Cinque Terre</h1>
+  <h3>Resize the browser window</h3>
+</div>
 </body>
 </html>
