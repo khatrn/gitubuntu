84ec25ef784fb46fa0387213334edf8faffd6dfe
khatn <kha.tn@neo-lab.vn>
2020-12-18 16:58:03 +0700

    [add][master]Create menu

--- test.php
+++ test.php
@@ -3,9 +3,17 @@
 <head>
 </head>
 <body style="font-family:Verdana;">
-<div style="background-color:#f1f1f1;padding:15px;">
-  <h1>Cinque Terre</h1>
-  <h3>Resize the browser window</h3>
-</div>
+  <div style="background-color:#f1f1f1;padding:15px;">
+    <h1>Cinque Terre</h1>
+    <h3>Resize the browser window</h3>
+  </div>
+  <div style="overflow:auto">
+    <div class="menu">
+      <div class="menuitem">The Walk</div>
+      <div class="menuitem">Transport</div>
+      <div class="menuitem">History</div>
+      <div class="menuitem">Gallery</div>
+    </div>
+  </div>
 </body>
 </html>
