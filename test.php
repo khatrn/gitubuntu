59bb75b132d0d1d47443cfb8f01815111787ee85
khatn <kha.tn@neo-lab.vn>
2020-12-18 17:18:41 +0700

    [Add][master]Add last paragraph

--- test.php
+++ test.php
@@ -23,6 +23,7 @@
         <p>The walk from Monterosso to Riomaggiore will take you approximately two hours, give or take an hour depending on the weather conditions and your physical shape.</p>
         <img src="img_5terre.jpg" style="width:100%">
     </div>
+    <div style="background-color:#f1f1f1;text-align:center;padding:10px;margin-top:7px;font-size:12px;"> This web page is a part of a demonstration of fluid web design made by w3schools.com. Resize the browser window to see the content respond to the resizing.</div>
   </div>
 </body>
 </html>
