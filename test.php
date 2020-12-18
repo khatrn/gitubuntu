53aa33b7b14338ca4b034ab9625b4fd51daab18a
khatn <kha.tn@neo-lab.vn>
2020-12-18 17:00:46 +0700

    [add][master]Create main

--- test.php
+++ test.php
@@ -7,5 +7,10 @@
   <h1>Cinque Terre</h1>
   <h3>Resize the browser window</h3>
 </div>
+<div class="main">
+    <h2>The Walk</h2>
+    <p>The walk from Monterosso to Riomaggiore will take you approximately two hours, give or take an hour depending on the weather conditions and your physical shape.</p>
+    <img src="img_5terre.jpg" style="width:100%">
+  </div>
 </body>
 </html>
